5402277c1dc1e8303729684cd3bee2da5781082a
Askariiiii <aj@uxmbg.com.au>
2025-12-10 10:10:49 +0800

    added few section in the page

---
+++ components/sections/AccidentReplacementVehicle/AccidentReplacementVehicleSection.js
@@ -0,0 +1,28 @@
+import TextLeftImageRight from '../../common/TextLeftImageRight'
+import TextRightImageLeft from '../../common/TextRightImageLeft'
+
+
+const AccidentReplacementVehicleSection = () => {
+  return (
+    <div className='pt-10'> 
+        <TextLeftImageRight 
+            title="We Deliver Accident Replacement Vehicles"
+            heading="Were you not at fault for the Accident and urgently require a replacement vehicle?"
+            content="We understand how hard it is for our clients when their car has suddenly been involved in an accident. We have a big fleet of Accident Replacement Vehicles around Australia that are ready to be delivered to you.
+                    Contact our team today and we’ll immediately organise for an Accident Replacement Car to be delivered to you on the Same Day."
+            image="/"
+        />
+
+        <TextRightImageLeft 
+            title="Our Replacement Vehicles Are Clean and COVID Safe"
+            heading="All our replacement cars are cleaned and disinfected after each use."
+            content="Given the circumstances around the world during the Covid Pandemic, it’s our duty to ensure our vehicles are free from any bacteria and viruses for the next client.
+                    We take all the necessary precautions to reduce the risk of infection for both our clients and staff when using our Accident Replacement Cars."
+            image="/"
+
+        />
+    </div>
+  )
+}
+
+export default AccidentReplacementVehicleSection
